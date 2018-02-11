272a5aa8e6ebec403d83be69b81c4fd173d674ea
漫步小老头 <1126281729@qq.com>
2018-02-11 18:42:29 +0800

    菜单图标

--- src/views/upms/menu/index.vue
+++ src/views/upms/menu/index.vue
@@ -10,30 +10,40 @@
     <el-row :gutter="10">
       <el-col class="org-tree-left">
         <el-card class="box-card">
-             <tree :list="treeData" id="menuTree"
-                   :open="false" choiceType="folder"
-                   @node-click="getNodeData"></tree>
+          <span style="font-size: 16px;font-family: '微软雅黑 Light';color:rgb(145,145,145)">┃ 菜单树</span>
+          <tree :list="treeData" id="menuTree"
+                :open="false" choiceType="folder"
+                @node-click="getNodeData"></tree>
         </el-card>
       </el-col>
-      <el-col :style="{width: (client.width-230) + 'px'}">
+      <el-col :style="{width: (client.width-250) + 'px'}">
         <el-card class="box-card">
           <el-form :label-position="labelPosition" label-width="80px" :model="form" ref="form">
-            <el-form-item label="类型" prop="type">
+            <!--<el-form-item label="类型" prop="type">
               <el-select class="filter-item" v-model="form.type"  :disabled="formEdit"  placeholder="请输入资源请求类型">
                 <el-option v-for="item in  typeOptions" :key="item" :label="item | typeFilter" :value="item"> </el-option>
               </el-select>
-            </el-form-item>
+            </el-form-item>-->
             <el-form-item label="父级节点" prop="parentId">
-              <el-input v-model="form.parentId" :disabled="formEdit" placeholder="请输入父级节点"></el-input>
+              <el-input v-model="form.parentName" disabled placeholder="请选择父级节点"></el-input>
             </el-form-item>
-            <el-form-item label="标题" prop="name">
-              <el-input v-model="form.name" :disabled="formEdit"  placeholder="请输入标题"></el-input>
+            <el-form-item label="菜单名字" prop="name">
+              <el-input v-model="form.name" :disabled="formEdit"  placeholder="请输入名字"></el-input>
             </el-form-item>
-            <el-form-item label="权限标识" v-show="form.type==1" prop="permission">
+            <el-form-item label="权限标识" prop="permission">
               <el-input v-model="form.permission" :disabled="formEdit" placeholder="请输入权限标识"></el-input>
             </el-form-item>
-            <el-form-item label="图标" v-show="form.type!==1" prop="icon">
-              <el-input v-model="form.icon" :disabled="formEdit" placeholder="请输入图标"></el-input>
+            <el-form-item label="图标" prop="icon">
+              <div style="position: relative" >
+                <a @click="iconDialog = true">1212</a>
+                <el-input v-model="form.icon"  placeholder="请选择图标"></el-input>
+                <div style="position: absolute;top: 0;right: 45px;font-size: 23px;">
+                  <svg-icon v-if="form.icon!=null" :icon-class="form.icon" />
+                </div>
+              </div>
+            </el-form-item>
+            <el-form-item label="路由" prop="path">
+              <el-input v-model="form.path" :disabled="formEdit" placeholder="请输入资源路径"></el-input>
             </el-form-item>
             <el-form-item label="资源路径" v-show="form.type==1" prop="url">
               <el-input v-model="form.url" :disabled="formEdit" placeholder="请输入资源路径"></el-input>
@@ -61,6 +71,37 @@
         </el-card>
       </el-col>
     </el-row>
+
+    <el-dialog :visible.sync="iconDialog" width="30%" height="100px">
+      <el-tabs type="card" >
+        <el-tab-pane >
+          <span slot="label"><i class="el-icon-menu"></i> 菜单</span>
+          <div class="icon-container">
+            <span class="icon-span" v-for="icon in icons" @click="iconSelected(icon)">
+              <svg-icon :icon-class="icon"  />
+            </span>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane >
+          <span slot="label"><i class="el-icon-more"></i> 补充</span>
+          <div class="icon-container">
+            <span class="icon-span" v-for="icon in icons" @click="iconSelected(icon)">
+              <svg-icon :icon-class="icon"  />
+            </span>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+
+      <!--<el-tabs type="border-card">
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-date"></i> 我的行程</span>
+          我的行程
+        </el-tab-pane>
+        <el-tab-pane label="消息中心">消息中心</el-tab-pane>
+        <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+      </el-tabs>-->
+    </el-dialog>
   </div>
 </template>
 
@@ -76,9 +117,15 @@
     data() {
       return {
         formEdit: true,
+        iconDialog: false,
         formStatus: '',
         typeOptions: ['0', '1'],
         methodOptions: ['GET', 'POST', 'PUT', 'DELETE'],
+        icons: ['404', 'eye', 'menu', 'password', 'user', 'users', 'org',
+          'eye', 'menu', 'password', 'user', 'users', 'org',
+          'eye', 'menu', 'password', 'user', 'users', 'org',
+          'eye', 'menu', 'password', 'user', 'users', 'org',
+          'eye', 'menu', 'password', 'user', 'users', 'org'],
         listQuery: {
           name: undefined
         },
@@ -89,6 +136,7 @@
           permission: undefined,
           name: undefined,
           parentId: undefined,
+          parentName: undefined,
           url: undefined,
           icon: undefined,
           sort: undefined,
@@ -111,11 +159,6 @@
         return typeMap[type]
       }
     },
-    updated: function() {
-      console.log('---------updated')
-      console.log(this.treeHeight)
-      console.log('---------updated')
-    },
     created() {
       this.getList()
     },
@@ -126,14 +169,6 @@
         'client'
       ])
     },
-    // mounted() {
-    //   const that = this
-    //   window.onresize = () => {
-    //     return (() => {
-    //       that.clientHeight = client.getHeight(84)
-    //     })()
-    //   }
-    // },
     methods: {
       getList() {
         fetchTree(this.listQuery).then(response => {
@@ -148,10 +183,20 @@
         if (!this.formEdit) {
           this.formStatus = 'update'
         }
-        getObj(data.id).then(response => {
-          this.form = response.data.data
-        })
+        if (data.parentId === -1) {
+          this.form = data
+          this.form.parentName = '无'
+        } else {
+          getObj(data.parentId).then(response => {
+            this.form = data
+            this.form.parentName = response.data.data.name
+          })
+        }
         this.currentId = data.id
+      },
+      iconSelected(icon) {
+        this.iconDialog = false
+        console.log(icon)
       },
       handlerEdit() {
         if (this.form.menuId) {
@@ -230,6 +275,21 @@
 <style scoped>
   @media screen and (max-height: 768px) {
     .menuTreeCard {height: 200px}
+  }
+  .icon-span{
+    font-size: 26px;
+    margin: 10px;
+    color: black;
+  }
+  .icon-container{
+    height: 250px;
+    overflow: auto;
+    margin-bottom: 10px;
+    padding: 15px;
+  }
+  .icon-container .icon-span:hover{
+    cursor: pointer;
+    color: #419dff;
   }
 </style>
 

3f6b33737a0f374f3225a8a4f3a29ce26fddcacb
Alan <1126281729@qq.com>
2018-03-23 16:31:54 +0800

    流程添加，修复树形组件返回多次bug，

---
+++ src/api/student/revisit-question.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+// 查询集合
+export function getQuestion(query) {
+  return request({
+    url: '/stu/revisitquestion/getPage',
+    method: 'get',
+    params: query
+  })
+}
+// 添加
+export function addObj(obj) {
+  return request({
+    url: '/stu/revisitquestion',
+    method: 'post',
+    data: obj
+  })
+}
+// 根据ID获取
+export function getObj(id) {
+  return request({
+    url: '/stu/revisitquestion/' + id,
+    method: 'get'
+  })
+}
+// 删除
+export function delObj(id) {
+  return request({
+    url: '/stu/revisitquestion/' + id,
+    method: 'delete'
+  })
+}
+// 更新
+export function putObj(obj) {
+  return request({
+    url: '/student/rvisitquestion',
+    method: 'put',
+    data: obj
+  })
+}

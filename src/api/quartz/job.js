dc6d49a503f685696a35649905acee2023d6cf97
Alan <1126281729@qq.com>
2018-04-25 11:14:53 +0800

    角色

---
+++ src/api/quartz/job.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+// 查询集合
+export function fetchList(query) {
+  return request({
+    url: '/quartz/job/getPage',
+    method: 'get',
+    params: query
+  })
+}
+// 添加
+export function addObj(obj) {
+  return request({
+    url: '/quartz/schedulejob',
+    method: 'post',
+    data: obj
+  })
+}
+// 根据ID获取
+export function getObj(id) {
+  return request({
+    url: '/quartz/job/' + id,
+    method: 'get'
+  })
+}
+// 删除
+export function delObj(id) {
+  return request({
+    url: '/quartz/schedulejob/' + id,
+    method: 'delete'
+  })
+}
+// 更新
+export function putObj(obj) {
+  return request({
+    url: '/quartz/job/update',
+    method: 'put',
+    data: obj
+  })
+}

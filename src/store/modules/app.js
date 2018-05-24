24633421648d36ff39ea51df96e1366eead3a4d4
Alan <1126281729@qq.com>
2018-05-24 20:50:52 +0800

    部署

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -7,7 +7,7 @@ const app = {
     },
     visitedViews: [],
     iFrameUrl: 'http://52.83.97.172',
-    motorcycleType: ['C1','C2','C3','C4','A1','A2','A3','B1','B2','D','E','F','M','N','P'],
+    motorcycleType: ['C1', 'C2', 'C3', 'C4', 'A1', 'A2', 'A3', 'B1', 'B2', 'D', 'E', 'F', 'M', 'N', 'P'],
     loading: false,
     spread: false,
     client: {

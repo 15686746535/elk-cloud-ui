2667aea6982bf8905bb319d81820c7529eff935e
Alan <1126281729@qq.com>
2018-05-24 20:36:02 +0800

    学员导入

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build   -    -report`
-    // Set to `true` or `false` to always turn it on or off
+    // Set to `true` or `false` t  o always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {

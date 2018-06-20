6d9d8305280b5c5279f1621fd6169d87622c08f8
Alan <1126281729@qq.com>
2018-06-20 12:10:36 +0800

    添加备案号

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,44 +1,57 @@
 <template>
   <div class="login-container">
-    <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
-      class="card-box login-form">
-      <h3 class="title">系统登录</h3>
-      <el-form-item prop="username">
+    <el-container>
+      <!--s-->
+      <el-container>
+        <el-container>
+          <el-main>
+            <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
+                     class="card-box login-form">
+              <h3 class="title">系统登录</h3>
+              <el-form-item prop="username">
         <span class="svg-container svg-container_login">
           <svg-icon icon-class="user" />
         </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="用户名" />
-      </el-form-item>
-      <el-form-item prop="password">
+                <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="用户名" />
+              </el-form-item>
+              <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password"></svg-icon>
         </span>
-        <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-          placeholder="密码"></el-input>
-          <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>
-      </el-form-item>
+                <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
+                          placeholder="密码"></el-input>
+                <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>
+              </el-form-item>
 
-      <input name="randomStr" type="hidden" v-model="loginForm.randomStr" />
-      <!--<el-form-item>-->
-        <!--<el-col :span="2">-->
-          <!--<span class="svg-container">-->
-          <!--&lt;!&ndash;<icon-svg icon-class="form"/>&ndash;&gt;-->
-        <!--</span>-->
-        <!--</el-col>-->
-        <!--<el-col :span="11">-->
-          <!--<el-input name="code" type="text" v-model="loginForm.code" autoComplete="on" placeholder="验证码"/>-->
-        <!--</el-col>-->
-        <!--<el-col :span="10" style="height: 52px;">-->
-          <!--<img :src="src" style="margin-top: 2px;border-radius: 3px;" @click="refreshCode"/>-->
-        <!--</el-col>-->
-      <!--</el-form-item>-->
+              <input name="randomStr" type="hidden" v-model="loginForm.randomStr" />
+              <!--<el-form-item>-->
+              <!--<el-col :span="2">-->
+              <!--<span class="svg-container">-->
+              <!--&lt;!&ndash;<icon-svg icon-class="form"/>&ndash;&gt;-->
+              <!--</span>-->
+              <!--</el-col>-->
+              <!--<el-col :span="11">-->
+              <!--<el-input name="code" type="text" v-model="loginForm.code" autoComplete="on" placeholder="验证码"/>-->
+              <!--</el-col>-->
+              <!--<el-col :span="10" style="height: 52px;">-->
+              <!--<img :src="src" style="margin-top: 2px;border-radius: 3px;" @click="refreshCode"/>-->
+              <!--</el-col>-->
+              <!--</el-form-item>-->
 
-      <el-form-item>
-        <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
-          登陆
-        </el-button>
-      </el-form-item>
-    </el-form>
+              <el-form-item>
+                <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+                  登陆
+                </el-button>
+              </el-form-item>
+            </el-form>
+          </el-main>
+          <el-footer style="position: fixed; bottom: 0; width: 100%; text-align: center;font-size: 12px;color: #fff;">
+            驾校管理系统 v3.1.1<br>&copy;2018 eluchn.com All Rights Reserved.
+            <a href="http://www.miitbeian.gov.cn/" target="_blank" rel="nofollow" style="text-decoration: underline;color: #fff;">渝ICP备17017040号-1<i class="c-icon-jgwablogo" ></i></a>
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -103,7 +116,7 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-  @import "src/styles/mixin.scss";
+  @import 'src/styles/mixin.scss';
   $bg:#2d3a4b;
   $dark_gray:#889aa4;
   $light_gray:#eee;
@@ -154,7 +167,7 @@ export default {
       font-weight: bold;
     }
     .login-form {
-      position: absolute;
+      /*position: absolute;*/
       left: 0;
       right: 0;
       width: 400px;
@@ -181,5 +194,17 @@ export default {
       right: 35px;
       bottom: 28px;
     }
+    .c-icon-jgwablogo {
+      position: relative;
+      top: 3px;
+      margin-right: 6px;
+      width: 14px;
+      height: 17px;
+      display: inline-block;
+      overflow: hidden;
+      background: url(image/icons_5859e57.png) no-repeat;
+      background-position: -623px -96px;
+    }
   }
+
 </style>

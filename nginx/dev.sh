83fe45f1ce2848af4d3f287f231f57532af94ccf
许繁 <i@erhuo.org>
2018-07-27 17:35:41 +0800

    自动替换域名

--- nginx/dev.sh
+++ nginx/dev.sh
@@ -1,2 +1,6 @@
 sed -i "s/www.eluchn.com/www.eluchn.com/g" ./nginx/nginx.conf
 sed -i "s/www.eluchn.com/www.eluchn.com/g" ./config/index.js
+npm install
+npm run build
+docker build -t elk-cloud-ui:latest .
+docker stop elkui&&docker rm elkui&&docker run -itd -p 80:80 -p 443:443 --restart always --name=elkui elk-cloud-ui

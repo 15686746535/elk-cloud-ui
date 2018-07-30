de9af268a503f52980bc0a46c860c99d2d12f195
Alan <1126281729@qq.com>
2018-07-30 19:28:58 +0800

    导出

--- src/views/modules/finance/costNote.vue
+++ src/views/modules/finance/costNote.vue
@@ -350,19 +350,20 @@
         })
       },
       download() {
-        downloadExcel(this.listQuery).then(response => {
-          console.log(response)
-          var time = new Date()
-          var blob = new Blob([response.data], { type: 'application/x-xls;charset=utf-8' })
-          var downloadElement = document.createElement('a')
-          var href = window.URL.createObjectURL(blob) // 创建下载的链接
-          downloadElement.href = href
-          downloadElement.download = '费用信息(' + time.toLocaleString() + ').xls' // 下载后文件名
-          document.body.appendChild(downloadElement)
-          downloadElement.click() // 点击下载
-          document.body.removeChild(downloadElement) // 下载完成移除元素
-          window.URL.revokeObjectURL(href) // 释放掉blob对象
-        })
+        this.$message.error('开发中...')
+        // downloadExcel(this.listQuery).then(response => {
+        //   console.log(response)
+        //   var time = new Date()
+        //   var blob = new Blob([response.data], { type: 'application/x-xls;charset=utf-8' })
+        //   var downloadElement = document.createElement('a')
+        //   var href = window.URL.createObjectURL(blob) // 创建下载的链接
+        //   downloadElement.href = href
+        //   downloadElement.download = '费用信息(' + time.toLocaleString() + ').xls' // 下载后文件名
+        //   document.body.appendChild(downloadElement)
+        //   downloadElement.click() // 点击下载
+        //   document.body.removeChild(downloadElement) // 下载完成移除元素
+        //   window.URL.revokeObjectURL(href) // 释放掉blob对象
+        // })
       },
       updateFinaceStateHandle(chargeid, state) {
         // console.log(dat,row)

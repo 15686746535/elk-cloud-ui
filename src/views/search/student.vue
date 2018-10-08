c0efc82b81dc653432b48123fab1ca1d90d9f297
PM_李先生 <qc_578cfe49dd1e40a095d147f7c03f3b5c@git.code.tencent.com>
2018-10-08 14:14:45 +0800

    精确查找

--- src/views/search/student.vue
+++ src/views/search/student.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="student" style="height: 100%;">
+  <div class="student-serch" style="height: 100%;">
     <el-card style="height: 100%">
       <div style="height: 80px;">
           <el-input @keyup.enter.native="getList" style="width: 300px;" class="filter-item" placeholder="完整的 姓名/电话/身份证" v-model="listQuery.accurate"></el-input>
@@ -22,7 +22,6 @@
             </el-row>
           </template>
         </el-table-column>
-
         <el-table-column align="center" label="个人信息" min-width="230px">
           <template slot-scope="scope" >
             <!-- 个人信息 -->
@@ -42,12 +41,11 @@
 
               <el-row :gutter="10">
                 <el-col :span="7" class="table_text">介绍人:</el-col>
-                <el-col :span="17" class="table_text">{{scope.row.introducers.replace(new RegExp('/','gm'), '、')}}</el-col>
+                <el-col :span="17" class="table_text">{{scope.row.introducers}}</el-col>
               </el-row>
             </el-col>
           </template>
         </el-table-column>
-
         <el-table-column align="center" label="入学信息" min-width="230px">
           <template slot-scope="scope">
             <el-col style=" line-height: 25px">
@@ -70,7 +68,6 @@
             </el-col>
           </template>
         </el-table-column>
-
         <el-table-column align="center" label="培训信息" min-width="230px">
           <template slot-scope="scope">
 
@@ -96,19 +93,17 @@
             </el-col>
           </template>
         </el-table-column>
-
         <el-table-column align="center" label="费用信息" min-width="230px">
           <template slot-scope="scope">
             <el-col style=" line-height: 25px">
               <el-row :gutter="10">
-                <el-col :span="7" class="table_text">服务项:</el-col>
-                <el-col :span="17" class="table_text">{{scope.row.serviceNotes.replace(new RegExp('/','gm'), '、')}}</el-col>
-              </el-row>
-
-              <el-row :gutter="10">
                 <el-col :span="7" class="table_text">应收金额:</el-col>
                 <el-col :span="17" class="table_text">{{scope.row.receivable}}</el-col>
               </el-row>
+              <!--<el-row :gutter="10">-->
+              <!--<el-col :span="7" class="table_text">已收金额:</el-col>-->
+              <!--<el-col :span="17" class="table_text">{{scope.row.receivable - scope.row.arrearage}}</el-col>-->
+              <!--</el-row>-->
 
               <el-row :gutter="10">
                 <el-col :span="7" class="table_text">欠费金额:</el-col>
@@ -117,7 +112,6 @@
             </el-col>
           </template>
         </el-table-column>
-
       </el-table>
       <div v-show="!listLoading" class="page-util">
         <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
@@ -190,5 +184,165 @@
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+  .student-serch{
+    .img{
+      width: 50px;
+      height: 50px;
+      padding: 0;
+      border-radius: 150px;
+      overflow: hidden;
+    }
+    .page-util{
+      margin-top: 20px;
+      position: fixed;
+      bottom: 0;
+      background-color: #fff;
+      z-index: 666;
+      padding: 20px 0;
+      width: 100%;
+      left: 0px;
+      border-top: 1px solid #ccc;
+    }
+    .clearfix:before,
+    .clearfix:after {
+      display: table;
+      content: "";
+    }
+    .clearfix:after {
+      clear: both
+    }
+    .title{
+      height: 40px;
+      line-height: 40px;
+      color:#495060;
+      background-color: #f5f7fa;
+      text-align: center;
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+    .text_css{
+      color:#495060;
+      font-size: 12px;
+      word-break:keep-all;/* 不换行 */
+      white-space:nowrap;/* 不换行 */
+      overflow:hidden;/* 内容超出宽度时隐藏超出部分的内容 */
+      text-overflow:ellipsis;/* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
+    }
+    .table_text{
+      color: #7c7c7c;font-size: 12px;
+      text-align: left;
+      word-break:keep-all;/* 不换行 */
+      white-space:nowrap;/* 不换行 */
+      overflow:hidden;/* 内容超出宽度时隐藏超出部分的内容 */
+      text-overflow:ellipsis;/* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
+    }
+    .avatar-uploader .el-upload {
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+      border-color: #409EFF;
+    }
+    .avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 140px;
+      height: 180px;
+      line-height: 180px;
+      text-align: center;
+    }
+    .avatar {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+    .avatar_img {
+      line-height: 180px;
+      position: relative;
+      margin: auto;
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      width: 140px;
+      height: 180px;
+      display: block;
+    }
+    .AddAvatar-uploader .el-upload {
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+    }
+    .AddAvatar-uploader .el-upload:hover {
+      border-color: #409EFF;
+    }
+    .AddAvatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 100px;
+      height: 130px;
+      line-height: 130px;
+      text-align: center;
+    }
+    .AddAvatar {
+      width: 100px;
+      height: 130px;
+      display: block;
+    }
+    .batchCss{
+      background-color: rgba(64,158,255,.1);
+      display: inline-block;
+      padding: 0 10px;
+      height: 32px;
+      line-height: 30px;
+      font-size: 12px;
+      color: #409eff;
+      border-radius: 4px;
+      box-sizing: border-box;
+      border: 1px solid rgba(64,158,255,.2);
+      white-space: nowrap;
+      cursor: pointer;
+    }
+    .batchCss_selected{
+      background-color: rgba(103,194,58,.1);
+      border-color: rgba(103,194,58,.2);
+      color: #67c23a;
+    }
+    .batchCss:hover{
+      background-color: rgba(103,194,58,.1);
+      border-color: rgba(103,194,58,.2);
+      color: #67c23a;
+    }
+    .carClassCss{
+      background-color: rgba(64,158,255,.1);
+      display: inline-block;
+      padding: 0 10px;
+      height: 32px;
+      line-height: 30px;
+      font-size: 12px;
+      color: #409eff;
+      border-radius: 4px;
+      box-sizing: border-box;
+      border: 1px solid rgba(64,158,255,.2);
+      white-space: nowrap;
+      cursor: pointer;
+    }
+    .carClassCss_selected{
+      background-color: rgba(103,194,58,.1);
+      border-color: rgba(103,194,58,.2);
+      color: #67c23a;
+    }
+    .carClassCss:hover{
+      background-color: rgba(103,194,58,.1);
+      border-color: rgba(103,194,58,.2);
+      color: #67c23a;
+    }
+    .supervise_type_4{
+      color: #67C23A;
+    }
+  }
 
 </style>

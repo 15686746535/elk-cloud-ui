670bbc62ddabb9b3fbc0d665394bcbce3dc183ee
RanSong <1126281729@qq.com>
2018-12-06 18:14:04 +0800

    目标

--- src/views/modules/upms/target.vue
+++ src/views/modules/upms/target.vue
@@ -2,21 +2,30 @@
   <div style="height: 100%">
     <el-card style="height: 100%">
       <el-input @keyup.enter.native="searchClick" style="width: 200px;" class="filter-item" placeholder="姓名" clearable v-model="listQuery.condition"></el-input>
+      <el-select style="width:200px;" v-model="listQuery.plan" clearable filterable placeholder="设置状态">
+        <el-option
+          v-for="item in plans"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+
       <el-button class="filter-item" type="primary"  icon="el-icon-search" style="margin-bottom: 15px" @click="searchClick">搜索</el-button>
-      <el-table :key='tableKey'  :height="tableHeight-220" :data="list" v-loading="listLoading" :stripe="true" :fit="false" element-loading-text="给我一点时间" border fit highlight-current-row style="width: 100%" >
+      <el-table :key='tableKey'  :height="tableHeight-220" @sort-change="sortable" :data="list" v-loading="listLoading" :stripe="true" :fit="false" element-loading-text="给我一点时间" border fit highlight-current-row style="width: 100%" >
         <el-table-column type="index" label="序号"  align="center" width="50"></el-table-column>
         <el-table-column align="center" label="姓名">
           <template slot-scope="scope">
             <span>{{scope.row.target}}</span>
           </template>
         </el-table-column>
-        <el-table-column align="center" label="本月目标">
+        <el-table-column align="center" label="本月目标" sortable="custom" prop="plan">
           <template slot-scope="scope">
             <span  v-if="scope.row.plan != null">{{scope.row.plan}}</span>
             <span  v-else>未设置</span>
           </template>
         </el-table-column>
-        <el-table-column align="center" label="本月完成">
+        <el-table-column align="center" label="本月完成" sortable="custom" prop="complete">
           <template slot-scope="scope">
             <span>{{scope.row.complete }}</span>
           </template>
@@ -61,9 +70,22 @@
         list: null,
         total: null,
         listLoading: true,
+        plans: [
+          {
+            label: '已设置',
+            value: 1
+          },
+          {
+            label: '未设置',
+            value: 2
+          }
+        ],
         listQuery: {
           page: 1,
           limit: 20,
+          plan: null,
+          sidx: null, // plan 2未设置  1已设置   排序字段 sidx  排序方式 order
+          order: null,
           condition: null
         },
         tableKey: 0
@@ -85,6 +107,16 @@
         if (!plan || isNaN(plan)) return '0%'
         if (!complete || isNaN(complete)) complete = 0
         return Math.round(complete / plan * 100) + '%'
+      },
+      sortable(data) {
+        // plan 1未设置  2已设置   排序字段 sidx  排序方式 order
+        var map = {
+          ascending: 'ASC',
+          descending: 'DESC'
+        }
+        this.listQuery.sidx = data.prop
+        this.listQuery.order = map[data.order]
+        this.getList()
       },
       savePlan() {
         this.$prompt('请输入本月计划', '设置我的本月计划', {
